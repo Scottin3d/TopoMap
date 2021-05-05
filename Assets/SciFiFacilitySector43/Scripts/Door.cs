23cae4f7196d38632589b53efc6e2ae940dd6683
Mingzhen Wang <wangmingzhenofficial@gmail.com>
2021-05-05 02:25:01 -0700

    3D texture

---
+++ Assets/SciFiFacilitySector43/Scripts/Door.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    
+
+    private int trDoorOpen = Animator.StringToHash("DoorOpen");
+    private int trDoorClose = Animator.StringToHash("DoorClose");
+    private Animator animator;
+    private AudioSource audioSource;
+
+	void Start() {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+	}
+
+
+	void OnTriggerEnter(Collider c) {
+        openDoor(c);
+    }
+
+	void OnTriggerExit(Collider c) {
+        closeDoor(c);
+	}
+
+    public void openDoor(Collider c) {
+        if (c.tag.Equals("GameController")) {
+            audioSource.Play();
+            animator.SetTrigger(trDoorOpen);
+
+        }
+    }
+    public void closeDoor(Collider c) {
+        if (c.tag.Equals("GameController")) {
+            audioSource.Play();
+            animator.SetTrigger(trDoorClose);
+        }
+    }
+
+
+}
---
+++ Assets/SciFiFacilitySector43/Scripts/DoorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    private Door door;
+
+    void Start()
+    {
+        door = GetComponentInParent<Door>();
+    }
+
+    void OnTriggerEnter(Collider c) {
+
+        door.openDoor(c);
+
+    }
+
+    void OnTriggerExit(Collider c) {
+        door.closeDoor(c);
+    }
+}
