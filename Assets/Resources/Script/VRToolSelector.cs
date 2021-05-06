b0acc51422c637309504f1556df88ac9b2e01255
jacobchesnut <jakeehapi@hotmail.com>
2021-05-06 11:03:44 -0700

    tool skeleton in place

---
+++ Assets/Resources/Script/VRToolSelector.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRToolSelector : MonoBehaviour
+{
+    //this is the VR Tool Selector class, it is intended to handle the VR Player choosing from the types of handheld
+    //tools they may need to use in or on the map. Currently this includes:
+    //the road system
+    //the marker system
+    //
+    //selection is done by facing the left hand plam-up, where a selection menu will display, the player will then
+    //swipe left or right to their desired tool, up and down for the variations of the tool, and grab to select the tool.
+    //
+    //here is a horizontal and vertical layout of the menu so far:
+    //
+    //
+    //
+    //
+    //
+    //
+    // None <-> Road <-> Marker
+
+    private GameObject leftHand = null; //left hand of the VR player
+
+    private bool isActive = false; //whether or not the menu should be active (left hand is palm-up)
+
+    public enum toolSelectionState
+    {
+        None,
+        Road,
+        Marker,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("delayInitialization");
+    }
+
+    private void StartUp()
+    {
+        leftHand = VRStartupController.VRPlayerObject.transform.Find("SteamVRObjects/LeftHand").gameObject;
+    }
+
+    IEnumerator delayInitialization()
+    {
+        while(VRStartupController.VRPlayerObject == null)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        StartUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
