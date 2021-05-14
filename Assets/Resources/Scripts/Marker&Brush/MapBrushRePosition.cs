311cc0c6c1d4e2069dbba1f8584cef695c46b92e
Mingzhen Wang <wangmingzhenofficial@gmail.com>
2021-05-14 14:05:12 -0700

    Route fix

--- Assets/Resources/Scripts/Marker&Brush/MapBrushRePosition.cs
+++ Assets/Resources/Scripts/Marker&Brush/MapBrushRePosition.cs
@@ -8,18 +8,15 @@ public class MapBrushRePosition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        RepositionRoute();
+    }
+
+    IEnumerator RepositionRoute()
+    {
+        yield return new WaitForSeconds(0.05f);
         RaycastHit Hit;
         if (Physics.Raycast(this.transform.position, Vector3.down, out Hit))
         {
-            //if (Hit.collider.tag == "Chunk")
-            //{
-            //    Vector3 NewPosition = new Vector3(this.transform.position.x, Hit.point.y - 2.2f, this.transform.position.z);
-            //    this.transform.position = NewPosition;
-            //    this.GetComponent<ASL.ASLObject>().SendAndSetClaim(() =>
-            //    {
-            //        this.GetComponent<ASL.ASLObject>().SendAndSetWorldPosition(NewPosition);
-            //    });
-            //}
             Vector3 NewPosition = new Vector3(this.transform.position.x, Hit.point.y - 2.2f, this.transform.position.z);
             this.transform.position = NewPosition;
             this.GetComponent<ASL.ASLObject>().SendAndSetClaim(() =>
