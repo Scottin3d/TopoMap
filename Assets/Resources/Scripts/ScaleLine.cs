3de59cf02d3a37b16e78c9c9fb635e9957b4546d
NS <73915342+Ramuterasu@users.noreply.github.com>
2021-05-29 18:38:58 -0700

    attempting to draw line in proper position
    ScreenToWorld on what should be different points gives same point after

--- Assets/Resources/Scripts/ScaleLine.cs
+++ Assets/Resources/Scripts/ScaleLine.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class ScaleLine : MonoBehaviour
 {
     public static ScaleLine _sl;
-    public Canvas myCanvas;
-    public RectTransform RenderPanel;
+    public RectTransform myCanvas, RenderPanel;
+    public LineRenderer centerLine, baseLine, branchM, branchF;
 
     private static Camera curDisplay;
     private static ChunkData closestChunk;
@@ -22,6 +22,10 @@ public class ScaleLine : MonoBehaviour
     {
         Debug.Assert(myCanvas != null);
         Debug.Assert(RenderPanel != null);
+        Debug.Assert(centerLine != null);
+        Debug.Assert(baseLine != null);
+        Debug.Assert(branchM != null);
+        Debug.Assert(branchF != null);
         StartCoroutine(UpdateScaleLine());
     }
 
@@ -33,16 +37,45 @@ public class ScaleLine : MonoBehaviour
 
     IEnumerator UpdateScaleLine()
     {
+        Debug.Log(myCanvas.rect.height / 3f);
+        Debug.Log(myCanvas.rect.height *.3f);
         while (true)
         {
+            //get height of canvas
+            float canvasHeight = myCanvas.rect.height;
+            //get height, width of line panel
+            float panelHeight = canvasHeight / 3f;
+            float panelWidth = 50f;
+            float panelX = 30f;
+            float panelY = canvasHeight * 0.3f;
+            //get center, top/bottom of line panel
+            RenderPanel.rect.Set(panelX, panelY, panelWidth, panelHeight);
+
+            //get positions, convert
+            //Vector2 topPos = new Vector2(panelX + 0.5f * panelWidth, panelY);
+            //Vector2 botPos = new Vector2(panelX + 0.5f * panelWidth, panelY + panelHeight);
+            if(curDisplay != null)
+            {
+                Vector3 topPos = curDisplay.ScreenToWorldPoint(new Vector2(panelX + 0.5f * panelWidth, panelY));
+                Vector3 botPos = curDisplay.ScreenToWorldPoint(new Vector2(panelX + 0.5f * panelWidth, panelY + panelHeight));
+                Vector3[] cPosArr = { topPos, botPos };
+                Debug.Log("" + cPosArr[0] + "," + cPosArr[1]);
+                centerLine.SetPositions(cPosArr);
+            }
+            
+
+
             if (closestChunk != null)
             {
-                Debug.Log("Name: " + closestChunk.gameObject.name + " with parent: " + closestChunk.gameObject.transform.parent);
-                //get height of canvas
-                //get height, width of line panel
-                //get center, top/bottom of line panel
+                //Debug.Log("Name: " + closestChunk.gameObject.name + " with parent: " + closestChunk.gameObject.transform.parent);
                 //get parent of chunk
+                Transform chunkParent = closestChunk.transform.parent;
                 //get heightmap size
+                float mapSize = -1f;
+                if (chunkParent != null)
+                {
+                    mapSize = chunkParent.gameObject.GetComponent<GenerateMapFromHeightMap>().mapSize;
+                }
 
                 //get scale between canvas height and panel height
                 //draw line from top to bottom edge of line panel, through center
@@ -93,8 +126,7 @@ public class ScaleLine : MonoBehaviour
                 //if we hit 1 chunk, out closest chunk is now that chunk
                 //if we hit 2 chunks, determine the closest chunk
                 switch (castInfo)
-                {
-                    
+                {                    
                     case 3: closestChunk = (hitC.distance >= hitM.distance) ? closestChunk : mChunk;
                         break;
                     case 2: closestChunk = mChunk;
